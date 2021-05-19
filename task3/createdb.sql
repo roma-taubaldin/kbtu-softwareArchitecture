4c19bdff37f4041d9d12dda0fc32aa74f3aa7996
roma-taubaldin <roma.taubaldin@gmail.com>
2021-05-19 22:21:00 +0600

    Added sql file of task3

---
+++ task3/createdb.sql
@@ -0,0 +1,3 @@
+create schema kbtu;
+create sequence kbtu.tasks_id_seq;
+create table kbtu.tasks (id int not null default nextval('kbtu.tasks_id_seq'), task varchar(500) not null, correct_date timestamp not null default now());
